ad9fe5e806f3934d14205bd051df2a3beaf4f830
Dennis Kamau Ngera <ngerakam123@gmail.com>
2022-10-17 22:13:56 +0300

    setup commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const mwaka = new Date().getFullYear();
+
+const Footer = () => {
+    return ( <footer><p>Copyright ⓒ {mwaka}</p></footer> );
+}
+ 
+export default Footer;
